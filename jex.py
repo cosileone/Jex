b9c741c38857c3967029e4cd545e0c043a72221c
Cosimo Leone <cosi.leone@icloud.com>
2016-03-20 20:09:05 -0400

    whitespace fixes some imports changed in jex main

--- jex.py
+++ jex.py
@@ -1,62 +1,67 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
-import sqlite3
+# import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "jex is awesome"
-app.database = "jex.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./users.db'
+
+db = SQLAlchemy(app)
+
+from models import *
+
 
 #login required decorator
 def login_required(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			flash("wut")
-			return f(*args, **kwargs)
-		else:
-			flash("Required: You need to login first.")
-			return redirect(url_for('login'))
-	return wrap
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Required: You need to login first.")
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/welcome')
 @login_required
 def welcome():
-	flash("Welcome to Jex!")
-	return render_template("welcome.html")
+    flash("Welcome to Jex!")
+    return render_template("welcome.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	g.db = connect_db()
-	cur = g.db.execute('select * from users')
-	users = cur.fetchall()
-	g.db.close()
+    users = db.session.query(User).all()
 
-	error = None
-	if request.method == 'POST':
-		if request.form['email'] != 'admin' or request.form['passwd'] != 'admin':
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			session['logged_in'] = True
-			flash("Success: You have been logged in.")
-			return redirect(url_for('welcome'))
-	return render_template('login.html', error=error, users=users)
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] != 'admin' or request.form['passwd'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash("Success: You have been logged in.")
+            return redirect(url_for('welcome'))
+    return render_template('login.html', error=error, users=users)
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash("Alert: You have been logged out.")
-	return redirect(url_for('home'))
+    session.pop('logged_in', None)
+    flash("Alert: You have been logged out.")
+    return redirect(url_for('home'))
 
-def connect_db():
-	return sqlite3.connect(app.database)
+# def connect_db():
+# 	return sqlite3.connect('users.db')
 
 if __name__ == '__main__':
-	host = "127.0.0.1"
-	port = 8391
-	app.debug = True
-	app.run(host, port)
+    host = "127.0.0.1"
+    port = 8391
+    app.debug = True
+    app.run(host, port)
